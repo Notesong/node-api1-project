2a34254d434129e250befe4b9ef7da13493deb97
Lee Damien <notesong.git@outlook.com>
2020-03-11 22:19:48 -0400

    Add user put, patch, and get by id

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const server = express();
 // data store (replaces a DB)
 let users = [];
 
+// set server port for it to listen on
 server.listen(4000, () => {
     console.log('Listening on port 4000...')
 });
@@ -58,5 +59,101 @@ server.post('/users', (req, res) => {
 // handler for DELETE '/users/:id'
 //
 server.delete('/users/:id', (req, res) => {
+    // get the user id from the request params
+    const { id } = req.params;
 
+    // if the user is in the database, set it to deleted
+    const deleted = users.find(user => user.id === id);
+
+    // if the user is found
+    if (deleted) {
+        //filter the user out of users array
+        users = users.filter(user => user.id !== id);
+
+        // send back the deleted user
+        res.status(200).json(deleted);
+
+    // if the user isn't found, return a 404 error message
+    } else {
+        res
+            .status(404)
+            .json({ success: false, message: "user id not found" })
+    }
+});
+
+//
+// handler for PUT '/users/:id'
+//
+server.put('/users/:id', (req, res) => {
+    // get the user id from the request body
+    const { id } = req.params;
+
+    // get the user changes from the request body
+    const changes = req.body;
+
+    // find the user's index in users array
+    let index = users.findIndex(user => user.id === id);
+
+    // if the user is found
+    if (index !== -1) {
+        // set the changes to the user based on the user's index
+        users[index] = changes;
+
+        // send back the edited user
+        res.status(200).json(users[index]);
+
+    // if the user is not found, return a 404 error message
+    } else {
+        res
+            .status(404)
+            .json({ success: false, message: "user id not found" });
+    }
+});
+
+//
+// handler for PATCH '/users/:id'
+//
+server.patch('/users/:id', (req, res) => {
+    // get the user id from the request body
+    const { id } = req.params;
+
+    // get the user changes from the request body
+    const changes = req.body;
+
+    // if the user is found in users array, set it to found
+    let found = users.find(user => user.id === id);
+
+    // if the user was found
+    if (found) {
+        // Object.assign() updates users array using the changes
+        Object.assign(found, changes);
+
+        // send the found user back
+        res.status(200).json(found);
+    // if the user was not found, return a 404 error message
+    } else {
+        res
+            .status(404)
+            .json({ success: false, message: "user id not found" })   
+    }
+});
+
+//
+// handler for GET '/users/:id'
+//
+server.get('/users/:id', (req, res) => {
+    // get the user id from the request body
+    const { id } = req.params;
+    
+    const found = users.find(user => user.id === id);
+
+    // if the user is found based on id, return the user
+    if (found) {
+        res.status(200).json(found);
+    // if the user isn't found, return a 404 error message
+    } else {
+        res
+            .status(404)
+            .json({ success: false, message: 'user not found'});
+    }
 });
