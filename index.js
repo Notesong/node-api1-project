4cc6ac760b16356dc423da9421a94a32f0cf9c8f
Lee Damien <notesong.git@outlook.com>
2020-03-15 19:22:37 -0400

    Formatting adjustment

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // brings express into the project
-const express = require('express');
+const express = require("express");
 // generates unique, random, url-friendly id's
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 // create a server object
 const server = express();
 
 // data store (replaces a DB)
 let users = [
-    {
-        id: shortid.generate(),
-        name: "Jane Doe",
-        bio: "Not Tarzan's Wife, another Jane.",
-    },
-    {
-        id: shortid.generate(),
-        name: "John Doe",
-        bio: "I'm just a guy.",
-    }
+  {
+    id: shortid.generate(),
+    name: "Jane Doe",
+    bio: "Not Tarzan's Wife, another Jane."
+  },
+  {
+    id: shortid.generate(),
+    name: "John Doe",
+    bio: "I'm just a guy."
+  }
 ];
 
 // set server port for it to listen on
 server.listen(4000, () => {
-    console.log('Listening on port 4000...')
+  console.log("Listening on port 4000...");
 });
 
 // parses request's body text into an object for req.body
@@ -38,164 +38,171 @@ server.use(express.json());
 //
 // handler for GET '/'
 //
-server.get('/', (req, res) => {
-    res.send('Hello World!');
+server.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 //
 // handler for GET '/api'
 //
-server.get('/api', (req, res) => {
-    res.send('Hello API!');
+server.get("/api", (req, res) => {
+  res.send("Hello API!");
 });
 
 //
 // handler for GET '/api/users'
 // returns all users
 //
-server.get('/api/users', (req, res) => {
-    res.status(200).json(users);
+server.get("/api/users", (req, res) => {
+  res.status(200).json({ success: true, users });
 });
 
 //
 // handler for GET '/api/users/:id'
 // returns a specific user based on id
 //
-server.get('/api/users/:id', (req, res) => {
-    // get the user id from the request body
-    const { id } = req.params;
-    
-    const found = users.find(user => user.id === id);
+server.get("/api/users/:id", (req, res) => {
+  // get the user id from the request body
+  const { id } = req.params;
 
-    // if the user is found based on id, return the user
-    if (found) {
-        res.status(200).json(found);
+  const found = users.find(user => user.id === id);
+
+  // if the user is found based on id, return the user
+  if (found) {
+    res.status(200).json({ success: true, found });
 
     // if the user isn't found, return a 404 error message
-    } else {
-        res
-            .status(404)
-            .json({ success: false, message: 'The user with the specified ID does not exist.'});
-    }
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "The user with the specified ID does not exist."
+    });
+  }
 });
 
 //
 // handler for POST '/api/users'
 //
-server.post('/api/users', (req, res) => {
-    try {
-        // get the new user info from the request body
-        const userInfo = req.body;
+server.post("/api/users", (req, res) => {
+  try {
+    // get the new user info from the request body
+    const userInfo = req.body;
 
-        if (!userInfo.name || !userInfo.bio) {
-            res
-                .status(400)
-                .json({ success: false, message: "Please provide a name and bio for the user." })
-        } else {
-            // creates a user id using shortid
-            userInfo.id = shortid.generate();
+    if (!userInfo.name || !userInfo.bio) {
+      res.status(400).json({
+        success: false,
+        message: "Please provide a name and bio for the user."
+      });
+    } else {
+      // creates a user id using shortid
+      userInfo.id = shortid.generate();
 
-            // push the new user onto users array
-            users.push(userInfo);
+      // push the new user onto users array
+      users.push(userInfo);
 
-            // send back updated users
-            res.status(201).json(users);
-        }
-    } catch (error) {
-        res
-            .status(500)
-            .json({ success: false, message: "There was an internal error while saving the user." });
+      // send back updated users
+      res.status(201).json({ success: true, users });
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "There was an internal error while saving the user."
+    });
+  }
 });
 
 //
 // handler for DELETE '/api/users/:id'
 //
-server.delete('/api/users/:id', (req, res) => {
-    // get the user id from the request params
-    const { id } = req.params;
+server.delete("/api/users/:id", (req, res) => {
+  // get the user id from the request params
+  const { id } = req.params;
 
-    // if the user is in the database, set it to deleted
-    const deleted = users.find(user => user.id === id);
+  // if the user is in the database, set it to deleted
+  const deleted = users.find(user => user.id === id);
 
-    // if the user is found
-    if (deleted) {
-        //filter the user out of users array
-        users = users.filter(user => user.id !== id);
+  // if the user is found
+  if (deleted) {
+    //filter the user out of users array
+    users = users.filter(user => user.id !== id);
 
-        // send back the deleted user
-        res.status(200).json(deleted);
+    // send back the deleted user
+    res.status(200).json({ success: true, deleted });
 
     // if the user isn't found, return a 404 error message
-    } else {
-        res
-            .status(404)
-            .json({ success: false, message: "The user with the specified ID does not exist." })
-    }
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "The user with the specified ID does not exist."
+    });
+  }
 });
 
 //
 // handler for PUT '/api/users/:id'
 //
-server.put('/api/users/:id', (req, res) => {
-    // get the user id from the request body
-    const { id } = req.params;
+server.put("/api/users/:id", (req, res) => {
+  // get the user id from the request body
+  const { id } = req.params;
 
-    // get the user changes from the request body
-    const changes = req.body;
+  // get the user changes from the request body
+  const changes = req.body;
 
-    if (!changes.name || !changes.bio) {
-        res
-            .status(400)
-            .json({ success: false, message: "Please provide a name and bio for the user." })
+  if (!changes.name || !changes.bio) {
+    res.status(400).json({
+      success: false,
+      message: "Please provide a name and bio for the user."
+    });
+  } else {
+    // set the user id based off the params
+    changes.id = id;
+
+    // find the user's index in users array
+    let index = users.findIndex(user => user.id === id);
+
+    // if the user is found
+    if (index !== -1) {
+      // set the changes to the user based on the user's index
+      users[index] = changes;
+
+      // send back the edited user
+      res.status(200).json(users[index]);
+
+      // if the user is not found, return a 404 error message
     } else {
-        // set the user id based off the params
-        changes.id = id;
-
-        // find the user's index in users array
-        let index = users.findIndex(user => user.id === id);
-
-        // if the user is found
-        if (index !== -1) {
-            // set the changes to the user based on the user's index
-            users[index] = changes;
-
-            // send back the edited user
-            res.status(200).json(users[index]);
-
-        // if the user is not found, return a 404 error message
-        } else {
-            res
-                .status(404)
-                .json({ success: false, message: "The user with the specified ID does not exist." });
-        }
+      res.status(404).json({
+        success: false,
+        message: "The user with the specified ID does not exist."
+      });
     }
+  }
 });
 
 //
 // handler for PATCH '/api/users/:id'
 //
-server.patch('/api/users/:id', (req, res) => {
-    // get the user id from the request body
-    const { id } = req.params;
+server.patch("/api/users/:id", (req, res) => {
+  // get the user id from the request body
+  const { id } = req.params;
 
-    // get the user changes from the request body
-    const changes = req.body;
+  // get the user changes from the request body
+  const changes = req.body;
 
-    // if the user is found in users array, set it to found
-    let found = users.find(user => user.id === id);
+  // if the user is found in users array, set it to found
+  let found = users.find(user => user.id === id);
 
-    // if the user was found
-    if (found) {
-        // Object.assign() updates users array using the changes
-        Object.assign(found, changes);
+  // if the user was found
+  if (found) {
+    // Object.assign() updates users array using the changes
+    Object.assign(found, changes);
 
-        // send the found user back
-        res.status(200).json(found);
+    // send the found user back
+    res.status(200).json({ success: true, found });
     // if the user was not found, return a 404 error message
-    } else {
-        res
-            .status(404)
-            .json({ success: false, message: "The user with the specified ID does not exist." })   
-    }
+  } else {
+    res.status(404).json({
+      success: false,
+      message: "The user with the specified ID does not exist."
+    });
+  }
 });
